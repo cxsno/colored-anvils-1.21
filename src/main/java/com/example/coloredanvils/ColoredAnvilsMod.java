ecd33cdba1ee8f76300a8891981d560405853fac
cxsno <jkarimigho860757@gmail.com>
2026-04-15 19:43:20 +0100

    Create ColoredAnvilsMod.java

---
+++ src/main/java/com/example/coloredanvils/ColoredAnvilsMod.java
@@ -0,0 +1,10 @@
+package com.example.coloredanvils;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ColoredAnvilsMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Colored Anvils loaded");
+    }
+}
